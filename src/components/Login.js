c2f20dbd7456d701fb3ce0ab31d461ba879ce415
Vinit Patidar <117593724+Vinit-patidar-03@users.noreply.github.com>
2023-07-23 19:03:28 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,7 +9,7 @@ function Login(props) {
       const submit = async (e)=>
       {
          e.preventDefault();
-         const response = await fetch(`http://localhost:5000/api/auth/login`, {
+         const response = await fetch(`https://notebookapi-production.up.railway.app/login`, {
             method: "POST",
             headers: {
               "Content-Type": "application/json"
